d435adb946249732c523d267b2e239df4052dbe9
ryan nichols <rynodivino@gmail.com>
2017-02-26 21:54:30 -0800

    Remove comment

--- tests/unit/api/delete.js
+++ tests/unit/api/delete.js
@@ -2,45 +2,6 @@ const redisMock = require('../../mocks/redisClient');
 const sinon = require('sinon');
 const t = require('tap');
 
-/**
- *
-'use strict';
-
-module.exports = (req, res, redisClient) => {
-    if (!req.body.id || !req.body.owner) {
-        res.status(400).json({
-            message: 'Missing required parameters.'
-        });
-    }
-    redisClient.get(req.body.id, (err, data) => {
-        if (data) {
-            const link = JSON.parse(data);
-            if (link.owner === req.body.owner) {
-                // need to async or promis these nested callbacks.
-                redisClient.del(link.id, (err, reply) => {
-                    if (reply !== undefined) {
-                        redisClient.hdel(req.body.owner, req.body.id, (err, reply) => {
-                            if (reply !== undefined) {
-                                res.status(200).json({msg: 'Success'});
-                            }
-                        });
-                    }
-                });
-                // delete owner hash as well.
-            } else {
-                res.status(401).json({
-                    msg: 'Failed.  Only owner can delete this link.'
-                });
-            }
-        } else {
-            res.status(401).json({
-                msg: 'Failed.  Link does not exist.'
-            });
-        }
-    }); 
-};
-*/
-
 let del, json, req, res, stub;
 
 t.beforeEach((done)=>{
