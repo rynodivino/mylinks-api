c8b7c40124bd0a622700c1a197b16d6145017960
ryan nichols <rynodivino@gmail.com>
2017-02-28 13:02:54 -0800

    Add coverage to delete method.

--- api/delete.js
+++ api/delete.js
@@ -1,37 +1,46 @@
 'use strict';
 
-module.exports = (req, res, redisClient) => {
-    if (!req.body.id || !req.body.owner) {
-        res.status(400).json({
-            message: 'Missing required parameters.'
-        });
-    } else {
-        redisClient.get(req.body.id, (err, data) => {
-            if (data) {
-                const link = JSON.parse(data);
-                if (link.owner === req.body.owner) {
-                    // need to async or promis these nested callbacks.
-                    redisClient.del(link.id, (err, reply) => {
-                        if (reply !== undefined) {
-                            redisClient.hdel(req.body.owner, req.body.id, (err, reply) => {
-                                if (reply !== undefined) {
-                                    res.status(200).json({msg: 'Success'});
-                                }
-                            });
-                        }
-                    });
-                    // delete owner hash as well.
-                } else {
-                    res.status(401).json({
-                        msg: 'Failed.  Only owner can delete this link.'
-                    });
-                }
+const fetchLink = require('./fetch-link');
+
+const getCallback = (req, res, client) => {
+    return (err, data) => {
+        if (data) {
+            const link = JSON.parse(data);
+            if (link.owner === req.params.owner) {
+                // need to async or promis these nested callbacks.
+                client.del(link.id, (err, reply) => {
+                    if (reply !== undefined) {
+                        client.hdel(req.params.owner, req.params.id, (err, reply) => {
+                            if (reply !== undefined) {
+                                res.status(200).json({message: 'Success'});
+                            }
+                        });
+                    }
+                });
+                // delete owner hash as well.
             } else {
                 res.status(401).json({
-                    msg: 'Failed.  Link does not exist.'
+                    message: 'Failed.  Only owner can delete this link.'
                 });
             }
-        });
-    }
-    return res.end();
+        } else {
+            res.status(401).json({
+                message: 'Failed.  Link does not exist.'
+            });
+        }
+    };
+};
+
+module.exports = {
+    del: (req, res, redisClient) => {
+        if (!req.params.id || !req.params.owner) {
+            res.status(400).json({
+                message: 'Missing required parameters.'
+            });
+        } else {
+            const cb = getCallback(req, res, redisClient);
+            fetchLink(redisClient, req.params.id, cb);
+        }
+    },
+    getCallback
 };
--- tests/unit/api/delete.js
+++ tests/unit/api/delete.js
@@ -1,7 +1,9 @@
+const mockery = require('mockery');
 const redisMock = require('../../mocks/redisClient');
 const sinon = require('sinon');
 const t = require('tap');
 
+const redisClient = redisMock.createClient();
 let del, json, req, res, stub;
 
 t.beforeEach((done)=>{
@@ -9,7 +11,7 @@ t.beforeEach((done)=>{
     stub = sinon.stub();
     stub.returns({json});
     req = {
-        body: {
+        params: {
             id: 'blah',
             owner: 'me'
         }
@@ -23,22 +25,57 @@ t.beforeEach((done)=>{
     done();
 });
 
-t.plan(2);
+t.plan(6);
 
 t.test('delete 400\'s is missing id.', function (tt) {
-    tt.plan(3);
-    req.body.id = null;
-    del(req, res, redisMock);
+    tt.plan(2);
+    req.params.id = null;
+    del.del(req, res, redisClient);
     tt.ok(res.status.calledWith(400));
     tt.ok(json.calledWith({message:'Missing required parameters.' }));
-    tt.ok(res.end.called);
 });
 
 t.test('delete 400\'s is missing owner.', function (tt) {
-    tt.plan(3);
-    req.body.owner = null;
-    del(req, res, redisMock);
+    tt.plan(2);
+    req.params.owner = null;
+    del.del(req, res, redisClient);
     tt.ok(res.status.calledWith(400));
     tt.ok(json.calledWith({message:'Missing required parameters.' }));
-    tt.ok(res.end.called);
+});
+
+t.test('delete:del call getCallback and fetchLink if id and owner present.', function (tt) {
+    tt.plan(2);
+    del.del(req, res, redisClient);
+    tt.ok(redisClient.get.called);
+    tt.ok(redisClient.get.calledWith(req.params.id));
+});
+
+t.test('delete:getCallback 401 if no data.', function (tt) {
+    tt.plan(2);
+    const cb = del.getCallback(req, res, redisClient);
+    cb(null, null);
+    tt.ok(res.status.calledWith(401));
+    tt.ok(json.calledWith({message:'Failed.  Link does not exist.'}));
+});
+
+t.test('delete:getCallback 401 if wrong owner.', function (tt) {
+    tt.plan(2);
+    const cb = del.getCallback(req, res, redisClient);
+    cb(null, JSON.stringify({
+        id: 'http://motherscratcher.com',
+        owner: 'bill roberts'
+    }));
+    tt.ok(res.status.calledWith(401));
+    tt.ok(json.calledWith({message:'Failed.  Only owner can delete this link.'}));
+});
+
+t.test('delete:getCallback 200 if successful.', function (tt) {
+    tt.plan(2);
+    const cb = del.getCallback(req, res, redisClient);
+    cb(null, JSON.stringify({
+        id: 'blah',
+        owner: 'me'
+    }));
+    tt.ok(res.status.calledWith(200));
+    tt.ok(json.calledWith({message:'Success'}));
 });
